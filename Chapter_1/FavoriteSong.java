1213527ec56b2b1f722a9899fc0dafcd081973cb
KeithDavidDalocanog-UCM <lumetrimain1@gmail.com>
2025-09-02 22:38:44 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Hindi na na-nanaginip, hindi na ma-makagising");
+        System.out.println("Pasindi na ng ilaw");
+        System.out.println("Minumulto na 'ko ng damdamin ko, ng damdamin ko");
+        System.out.println("Hindi pa ba sapat pagpapahirap sa 'kin? (Ng damdamin ko)");
+    }
+}
