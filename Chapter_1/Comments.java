4e697eedb597da178b221bf092637b0d994fca77
KeithDavidDalocanog-UCM <lumetrimain1@gmail.com>
2025-09-02 22:48:02 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+/**
+ * This is a Javadoc comment.
+ * It is used to generate official documentation for the code.
+ * This class serves as a demonstration of Java's comment types.
+ *
+ * @author Student
+ * @version 1.0
+ */
+public class Comments {
+    public static void main(String[] args) {
+        /*
+         * This is a multi-line comment.
+         * It can span several lines and is often used for longer
+         * explanations within a method or for temporarily disabling
+         * a block of code.
+         */
+        System.out.println("This program demonstrates Java comments."); // This is a single-line comment.
+    }
+}
