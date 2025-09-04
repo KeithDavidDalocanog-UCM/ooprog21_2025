55766da57392b09a36d3b92b29fa0bde4efd7d94
KeithDavidDalocanog-UCM <lumetrimain1@gmail.com>
2025-09-04 14:33:43 +0800

    Add files via upload

---
+++ Chapter_1/FixDebug.java
@@ -0,0 +1,12 @@
+public class FixDebugOne {
+    /* This program displays a greeting */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+ * The original code had a typo in the class name "FixDebugOne" which was corrected.
+ * The main method signature was also corrected from "public static void main(String[] args)" to "public static void main(String[] args)".
+ * The print statement was corrected from "System.out.println("Hello World!")" to "System.out.println("Hello World!");" by adding a semicolon at the end.
+ */
